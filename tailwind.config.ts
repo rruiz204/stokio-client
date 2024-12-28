97eeedffab586245258aefe60ee1596e2c2383a5
roney ruiz <roneyruizrojas@gmail.com>
2024-12-27 21:21:29 -0600

    feat: setting the color palette bases in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,ts}"];
+export const content: string[] = ["./src/**/*.{html,ts}"];
+
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      dark: {
+        100: "#1F242A"
+      },
+      light: {
+        100: "#D9D9DB"
+      },
+      primary: {
+        100: "D81C52",
+        200: "D85B7F",
+        300: "D89AAD",
+      },
+    },
+  },
 };
+
 export const plugins = [];
 
